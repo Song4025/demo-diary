74af1b49d0b40dde6895d8545722fbd8cc8f6c78
Song4025 <thdgus4025@gmail.com>
2023-03-27 23:01:01 +0900

    add item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import DiaryEditor from './DiaryEditor';
-import DiaryList from './DiaryList';
-import './App.css';
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+import "./App.css";
 
 const dummyList = [
     {
@@ -8,30 +8,30 @@ const dummyList = [
         author: "언젠가는",
         content: "사용할것이다.",
         emotion: 5,
-        created_date: new Date().getTime()
+        created_date: new Date().getTime(),
     },
     {
         id: 2,
         author: "jea",
         content: "you're my destiny",
         emotion: 4,
-        created_date: new Date().getTime()
+        created_date: new Date().getTime(),
     },
     {
         id: 3,
         author: "안녕",
         content: "hello1111",
         emotion: 3,
-        created_date: new Date().getTime()
+        created_date: new Date().getTime(),
     },
     {
         id: 4,
         author: "데이터11",
         content: "내용은 이것이다",
         emotion: 2,
-        created_date: new Date().getTime()
+        created_date: new Date().getTime(),
     },
-]
+];
 
 function App() {
     return (
